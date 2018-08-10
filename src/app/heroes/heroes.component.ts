481dfb7bbb7ae0e65f03fb6996e2fc5a65d70f70
zhangjing <zhangjing@getui.com>
2018-08-10 16:31:46 +0800

    基础

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROS } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    console.log(hero, 12)
+    this.selectedHero = hero
+  }
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => {
+      this.heroes = heroes;
+    })
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROS: Hero[] = [
+  { id: 11, name: 'aaa'},
+  { id: 12, name: 'bbb'},
+  { id: 13, name: 'ccc'},
+  { id: 14, name: 'ddd'},
+  { id: 15, name: 'eee'},
+]
